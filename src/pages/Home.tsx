92bb137d1a5c970470b7315b9d8ea90b18d60620
hui zheng <huizh365@gmail.com>
2025-04-10 14:40:38 +0200

    Added pics and products on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,43 @@
+import { Link } from "react-router"
 import { ProductList } from "../components/ProductList"
+import  chocolate  from "../assets/chocolate.png"
+import  candy  from "../assets/candy.png"
+import  nuts  from "../assets/nuts.png"
+import  driedFruit  from "../assets/dried-fruit.png"
+import easter from "../assets/easter-banner.png"
 
 export const Home = () => {
+
     return (
-        <>
+        <div className="home">
+          <div id="hero">
+            <img src={easter}/>
+            <span>HAPPY EASTER</span>
+          </div>
+          <div className="home-categories">
+            <Link to="/products?category=Chocolate" className="category-link">
+                <img src={chocolate} alt="chocolate"/>
+                <span className="category-text">Chocolate</span>
+            </Link>
+            <Link to="/products?category=Candy" className="category-link">
+                <img src={candy} alt="candy"/>
+                <span className="category-text">Candy</span>
+            </Link>
+            <Link to="/products?category=Nuts" className="category-link">
+                <img src={nuts} alt="nuts"/>
+                <span className="category-text">Nuts</span>
+            </Link>
+            <Link to="/products?category=Dried Fruit" className="category-link">
+                <img src={driedFruit} alt="dried fruit"/>
+                <span className="category-text">Dried fruit</span>
+            </Link>
+          </div>
+          <div className="title-container">
+            <h2 className="section-title">Recommended</h2>
+            <Link to="/products">More</Link>
+          </div>
+
         <ProductList limit={10} />
-        </>
+        </div>
     )
 }
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,22 +1,42 @@
 
 import "../styles/shop.css"
-
 import { ProductList } from "../components/ProductList"
-import { useState } from "react"
+import { useSearchParams } from "react-router"
 
 export const Products = () => {
-    const [category, setCategory] = useState<string>("all")
+    const [searchParams, setSearchParams] = useSearchParams()
+    const categoryURL = searchParams.get("category") ?? "all"
+
+    const handleFilter = (selectedFilter: string) => {
+        if (selectedFilter === "all") {
+            searchParams.delete("category")
+        } else {
+            searchParams.set("category", selectedFilter)
+        }
+        setSearchParams(searchParams)
+    }
 
     return (
         <div className="product-list-wrapper">
         <div className="filter-wrapper">
-            <button className="filter-btn" onClick={() => setCategory("all")}>All Snacks</button>
-            <button className="filter-btn" onClick={() => setCategory("Chocolate")}>Chocolate</button>
-            <button className="filter-btn" onClick={() => setCategory("Candy")}>Candy</button>
-            <button className="filter-btn" onClick={() => setCategory("Nuts")}>Nuts</button>
-            <button className="filter-btn" onClick={() => setCategory("Dried Fruit")}>Dried Fruit</button>
+            <button 
+                className={ categoryURL === "all" ? "active" :"filter-btn" }
+                onClick={() => handleFilter("all")}
+            >All Snacks</button>
+            <button 
+                className={ categoryURL === "Chocolate" ? "active" :"filter-btn" }
+                onClick={() => handleFilter("Chocolate")}
+            >Chocolate</button>
+            <button 
+                className={ categoryURL === "Candy" ? "active" :"filter-btn" }
+                onClick={() => handleFilter("Candy")}
+            >Candy</button>
+            <button 
+                className={ categoryURL === "Dried Fruit" ? "active" :"filter-btn" }
+                onClick={() => handleFilter("Dried Fruit")}
+            >Dried Fruit</button>
         </div>
-        <ProductList category={category} />
+        <ProductList category={categoryURL} />
         </div>
     )
 }
